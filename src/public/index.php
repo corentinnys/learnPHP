64da33129568762cbe222bed71795f13e76682c8
corentin nys <corentinys@gmail.com>
2023-08-02 16:25:03 +0200

    exercice 1->5 fini

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,5 @@
 <?php
-declare(strict_types=1);
+//declare(strict_types=1);
 
-echo 'It works !';
+//echo 'It works !';
+include ('variables/variable_types.php');
---
+++ src/public/variables/variable_types.php
@@ -0,0 +1,23 @@
+<?php
+// exercice 1
+$firstName="Corentin";
+echo "<p>je suis $firstName</p>";
+// exercice 2
+$age= 32;
+echo "<p>j'ai  $age ans</p>";
+// exercice 3
+$eyeColor= "brun";
+echo "<p>j'ai les yeux  $eyeColor</p>";
+// exercice 4
+// tableau associatif
+
+// exercice 5
+$family = array(
+    0 => 'Dominique ',
+    1 => 'Vincent',
+    2 => 'Corentin',
+    3 => 'Gwenael',
+    4 => 'loik - mael',
+    5=>'saozic');
+
+echo "<p>la première personne de ma famille est :$family[0]</p>";
